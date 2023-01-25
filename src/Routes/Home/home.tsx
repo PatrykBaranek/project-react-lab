658c7af9510c4bc689ba1628526a829baab4727c
patrykbaranek <cien2460045@gmail.com>
2023-01-25 19:08:07 +0100

    add few changes

--- src/Routes/Home/home.tsx
+++ src/Routes/Home/home.tsx
@@ -2,6 +2,10 @@ import { FC, useContext, useEffect } from 'react';
 import { AuthContext } from '../../context/Account/AccountContext';
 import { useNavigate } from 'react-router-dom';
 
+import './home.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faImage, faTable } from '@fortawesome/free-solid-svg-icons';
+
 export const Home: FC = () => {
 	const { state: user } = useContext(AuthContext);
 	const navigate = useNavigate();
@@ -13,8 +17,24 @@ export const Home: FC = () => {
 	}, [navigate, user]);
 
 	return (
-		<>
-			<p>home</p>
-		</>
+		<div className="home-container">
+			<div className="page-title">
+				<h1>Welcome {user.login?.username}!</h1>
+			</div>
+
+			<div className="post-album-links-container">
+				<div className="album-link">
+					<h3>See All Albums</h3>
+
+					<FontAwesomeIcon icon={faImage} onClick={() => navigate('/photos')} />
+				</div>
+
+				<div className="posts-link">
+					<h3>See All Posts</h3>
+
+					<FontAwesomeIcon icon={faTable} onClick={() => navigate('/posts')} />
+				</div>
+			</div>
+		</div>
 	);
 };
