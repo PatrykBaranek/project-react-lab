64c015f9cd81293f61ffbb49c7bc541b8486f272
patrykbaranek <cien2460045@gmail.com>
2023-02-24 01:15:26 +0100

    change types file to be dictionary

---
+++ src/Common/types.d.ts
@@ -0,0 +1,65 @@
+export interface IPost {
+	id: number;
+	title: string;
+	body: string;
+	userId: number;
+}
+
+export interface IComment {
+	postId: number;
+	id: number;
+	name: string;
+	email: string;
+	body: string;
+	title?: string;
+}
+
+export interface IPhoto {
+	albumId: number;
+	id: number;
+	thumbnailUrl: string;
+	title: string;
+	url: string;
+}
+
+export interface IAlbum {
+	userId: number;
+	id: number;
+	title: string;
+}
+
+export interface IAdress {
+	street: string;
+	suite: string;
+	city: string;
+	zipcode: string;
+	geo: {
+		lat: string;
+		lng: string;
+	};
+}
+
+export interface ICompany {
+	name: string;
+	catchPhrase: string;
+	bs: string;
+}
+
+export interface IUser {
+	id: number;
+	name: string;
+	username: string;
+	email: string;
+	adress: IAdress;
+	phone: string;
+	website: string;
+	company: ICompany;
+}
+
+export enum MethodType {
+	GET = 'GET',
+	POST = 'POST',
+	PUT = 'PUT',
+	PATCH = 'PATCH',
+	DELETE = 'DELETE',
+}
